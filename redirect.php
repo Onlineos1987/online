254f9e3ddab58a3e319155900888a510345d7e8b
Onlineos1987 <onlineos1987@gmail.com>
2025-06-16 16:58:09 +0500

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -7,5 +7,6 @@ $real_json_url = "https://raw.githubusercontent.com/Onlineos1987/online/main/upl
 
 // Returning a small json with the "real_url"
 echo json_encode(['real_url' => $real_json_url]);
+?>
 
 
