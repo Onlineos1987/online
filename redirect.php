b11b933c348c7858a5b7547799afabc3fb7223ac
Onlineos1987 <onlineos1987@gmail.com>
2025-06-17 09:29:13 +0500

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -3,7 +3,7 @@
 header('Content-Type: application/json');
 
 // Update this to whatever you want at any time
-$real_json_url = "https://raw.githubusercontent.com/Onlineos1987/online/main/uploads/Songs.json";
+$real_json_url = "https://raw.githubusercontent.com/musaoffice/TerrasolTechBestMusicMagicTiles/main/uploads/Songs.json";
 
 // Returning a small json with the "real_url"
 echo json_encode(['real_url' => $real_json_url]);
